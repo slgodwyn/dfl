eccbf147caf6398c11ae16b9c8cc8150b582859b
slgodwyn <79212430+slgodwyn@users.noreply.github.com>
2021-08-02 09:06:27 -0700

    Update vast.ai_vnc_setup.sh

--- vast.ai_vnc_setup.sh
+++ vast.ai_vnc_setup.sh
@@ -1,24 +1,94 @@
 #!/bin/bash
 
-echo "Run as root user"
+echo "Run as root or press ctrl+c to abort"
+echo "............................................................"
+echo "........................................."
+echo "......................"
+echo "......."
+echo "."
+echo "Proceeding with update, xfce4 desktop installation and VNC server configuration"
 
-apt update -y
-apt dist-upgrade -y
-apt-get install -y perl
-apt-get install -y sudo 
+apt-get update -y
+apt-get dist-upgrade -y
+apt-get install -y curl
+apt-get install -y git
 apt-get install -y wget
 apt-get install -y ffmpeg
 apt-get install -y unzip
-apt install xfce4 xfce4-goodies -y
-apt install tightvncserver -y
-apt-get install -y openssh-server
-/etc/init.d/ssh start
 
+if [ -f /usr/bin/sudo ] 
+then 
+    echo "Installing required apt packages ... [1/10]"
+else 
+    apt-get install sudo -y 
+    echo "Installing required apt packages ... [1/10]"
+fi
+if [ -f /usr/bin/openssh-server ] 
+then 
+    echo "Installing required apt packages ... [2/10]"
+Else 
+    apt-get install openssh-server -y 
+    echo "Installing required apt packages ... [2/10]"
+fi
+if [ -d /usr/share/locales ]
+then 
+    locale-gen en_US.UTF-8
+else
+    apt install -y locales
+    locale-gen en_US.UTF-8
+fi
+if [ -f /usr/bin/expect ] 
+then 
+    echo "Installing required apt packages ... [3/10]"
+else 
+    apt-get install expect -y 
+    echo "Installing required apt packages ... [3/10]"
+fi
+echo "Installing required apt packages ... [4/10]"
+apt-get install tightvncserver -y
+echo "Installing required apt packages ... [5/10]"
+echo "Installing required apt packages ... [6/10]"
+echo "Installing required apt packages ... [7/10]"
+if [ -f /usr/sbin/gdm3 ] 
+then 
+    echo "Installing required apt packages ... [8/10]"
+else 
+    apt-get install gdm3 -y 
+    echo "Installing required apt packages ... [8/10]"
+fi
+if [ -d /etc/X11 ]
+then
+    echo "/usr/sbin/gdm3" > /etc/X11/default-display-manager
+else
+    mkdir /etc/X11
+    echo "/usr/sbin/gdm3" > /etc/X11/default-display-manager
+    echo "Installing required apt packages ... [9/10]"
+    echo "Installing required apt packages ... [10/10]"
+fi
+if [ -f /usr/bin/xorg ] 
+then
+    echo "Installing required apt packages ... [9/10]"
+    echo "Installing required apt packages ... [10/10]"
+else
+    apt install -y xauth
+    apt install -y xorg openbox
+   
+fi
+if [ -F $HOME/.XAuthority ]
+then
+    mv $HOME/.XAuthority $HOME/.XAuthority_Backup
+else
+    touch $HOME/.XAuthority
+    xauth generate :0 . trusted 
+fi
+echo "set shared/default-x-display-manager gdm3" | debconf-communicate
+DEBIAN_FRONTEND=noninteractive apt install ubuntu-gnome-desktop -y
+
+echo "downloading latest Anaconda3 installation script"
 mkdir /tmp/DFL_install
 TMP_DIR="/tmp/DFL_install"
 DL_CONDA="https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh"
 DL_DFL="https://slgodwyn:holliday954@github.com/nagadit/DeepFaceLab_Linux.git"
-
 CONDA_PATHS=("/opt" "$HOME")
 CONDA_NAMES=("/ana" "/mini")
 CONDA_VERSIONS=("3" "2")
@@ -27,29 +97,23 @@ DIR_CONDA="$HOME/anaconda"
 CONDA_EXECUTABLE="${DIR_CONDA}/bin/conda"
 CONDA_TO_PATH=false
 ENV_NAME="deepfacelab"
-
 CONDA_EXECUTABLE="${DIR_CONDA}/bin/conda"
 CONDA_TO_PATH=true
-
-    # Download and install Anaconda3
 script_name='anaconda.sh'
 env_path='$HOME/anaconda'
-# download the installation script
-echo "Downloading conda installation script..."
 curl $DL_CONDA -s -o $script_name
 # create temporary conda installation
-echo "conda installation..."
+echo "Creating Conda environment configured for DeepFaceLab 2.0"
 TMPDIR=$TMP_DIR bash $script_name -b -f -p $env_path >> /dev/null
 rm $script_name
 
 source $DIR_CONDA/etc/profile.d/conda.sh
 $CONDA_EXECUTABLE init bash
-exec $SHELL
 
 cd $HOME
 git clone --depth 1 --no-single-branch "$DL_DFL"
 cd $HOME/DeepFaceLab_Linux
-git clone --depth 1 --no-single-branch https://slgodwyn:holliday954@github.com/iperov/DeepFaceLab.git
+git clone --depth 1 --no-single-branch https://github.com/slgodwyn/DeepFaceLab.git
 curl https://raw.githubusercontent.com/slgodwyn/dfl/main/dfl.yml -o $HOME/dfl.yml
 conda env create -f $HOME/dfl.yml
 
@@ -83,10 +147,19 @@ else
 fi
 
 cat << EOF > ~/.vnc/xstartup
-#!/bin/bash
+#!/bin/sh
+exec /etc/vnc/xstartup
 xrdb $HOME/.Xresources
-dbus-launch startxfce4 &
+vncconfig -iconic &
+dbus-launch --exit-with-session gnome-session &
 EOF 
+
+mkdir -p /etc/vncserver
+touch /etc/vncserver/vncservers.conf
+cat << EOF > /etc/vncserver/vncservers.conf
+VNCSERVERS="1:root"
+VNCSERVERARGS[1]="-geometry 1440x900 -depth 24"
+EOF
 
 awk '$1=="#AllowTcpForwarding"{foundLine=1; print "AllowTcpForwarding yes"} $1!="#AllowTcpForwarding"{print $0} END{if(foundLine!=1) print "permitRootLogin no"}' /etc/ssh/sshd_config > /etc/ssh/sshd_config.tmp && mv /etc/ssh/sshd_config.tmp /etc/ssh/sshd_config
 
